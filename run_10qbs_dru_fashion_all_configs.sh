bee55ebce7ac4fbd1613a0e55a220905a2aefa68
Hanlin Long <longhanlin818@outlook.com>
2023-06-05 19:21:51 +0800

    Create run_10qbs_dru_fashion_all_configs.sh

---
+++ run_10qbs_dru_fashion_all_configs.sh
@@ -0,0 +1,109 @@
+# dru 1_16 and 4_4
+# reference only
+nohup python mnist4x4_dru.py --qubits 10 --layers 4 --reuploads 4 --classes 2 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_2_4_4.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 4 --reuploads 4 --classes 3 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_3_4_4.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 4 --reuploads 4 --classes 4 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_4_4_4.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 4 --reuploads 4 --classes 5 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_5_4_4.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 4 --reuploads 4 --classes 6 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_6_4_4.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 4 --reuploads 4 --classes 7 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_7_4_4.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 4 --reuploads 4 --classes 8 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_8_4_4.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 4 --reuploads 4 --classes 9 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_9_4_4.log &
+sleep 1h
+nohup python mnist4x4_dru.py --qubits 10 --layers 16 --reuploads 1 --classes 2 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_2_1_16.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 16 --reuploads 1 --classes 3 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_3_1_16.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 16 --reuploads 1 --classes 4 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_4_1_16.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 16 --reuploads 1 --classes 5 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_5_1_16.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 16 --reuploads 1 --classes 6 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_6_1_16.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 16 --reuploads 1 --classes 7 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_7_1_16.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 16 --reuploads 1 --classes 8 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_8_1_16.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 16 --reuploads 1 --classes 9 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_9_1_16.log &
+sleep 1h
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 4 --reuploads 4 --classes 2 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_2_zzry_4_4.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 4 --reuploads 4 --classes 3 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_3_zzry_4_4.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 4 --reuploads 4 --classes 4 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_4_zzry_4_4.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 4 --reuploads 4 --classes 5 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_5_zzry_4_4.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 4 --reuploads 4 --classes 6 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_6_zzry_4_4.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 4 --reuploads 4 --classes 7 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_7_zzry_4_4.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 4 --reuploads 4 --classes 8 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_8_zzry_4_4.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 4 --reuploads 4 --classes 9 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_9_zzry_4_4.log &
+sleep 1h
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 16 --reuploads 1 --classes 2 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_2_zzry_1_16.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 16 --reuploads 1 --classes 3 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_3_zzry_1_16.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 16 --reuploads 1 --classes 4 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_4_zzry_1_16.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 16 --reuploads 1 --classes 5 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_5_zzry_1_16.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 16 --reuploads 1 --classes 6 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_6_zzry_1_16.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 16 --reuploads 1 --classes 7 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_7_zzry_1_16.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 16 --reuploads 1 --classes 8 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_8_zzry_1_16.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 16 --reuploads 1 --classes 9 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_9_zzry_1_16.log &
+sleep 1h
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 4 --reuploads 4 --classes 2 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_2_zxz_4_4.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 4 --reuploads 4 --classes 3 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_3_zxz_4_4.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 4 --reuploads 4 --classes 4 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_4_zxz_4_4.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 4 --reuploads 4 --classes 5 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_5_zxz_4_4.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 4 --reuploads 4 --classes 6 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_6_zxz_4_4.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 4 --reuploads 4 --classes 7 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_7_zxz_4_4.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 4 --reuploads 4 --classes 8 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_8_zxz_4_4.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 4 --reuploads 4 --classes 9 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_9_zxz_4_4.log &
+sleep 1h
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 16 --reuploads 1 --classes 2 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_2_zxz_1_16.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 16 --reuploads 1 --classes 3 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_3_zxz_1_16.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 16 --reuploads 1 --classes 4 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_4_zxz_1_16.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 16 --reuploads 1 --classes 5 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_5_zxz_1_16.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 16 --reuploads 1 --classes 6 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_6_zxz_1_16.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 16 --reuploads 1 --classes 7 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_7_zxz_1_16.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 16 --reuploads 1 --classes 8 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_8_zxz_1_16.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 16 --reuploads 1 --classes 9 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_9_zxz_1_16.log &
+sleep 1h
+nohup python mnist4x4_dru.py --qubits 10 --layers 8 --reuploads 4 --classes 2 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_2_4_8.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 8 --reuploads 4 --classes 3 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_3_4_8.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 8 --reuploads 4 --classes 4 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_4_4_8.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 8 --reuploads 4 --classes 5 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_5_4_8.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 8 --reuploads 4 --classes 6 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_6_4_8.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 8 --reuploads 4 --classes 7 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_7_4_8.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 8 --reuploads 4 --classes 8 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_8_4_8.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 8 --reuploads 4 --classes 9 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_9_4_8.log &
+sleep 1h
+nohup python mnist4x4_dru.py --qubits 10 --layers 32 --reuploads 1 --classes 2 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_2_1_32.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 32 --reuploads 1 --classes 3 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_3_1_32.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 32 --reuploads 1 --classes 4 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_4_1_32.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 32 --reuploads 1 --classes 5 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_5_1_32.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 32 --reuploads 1 --classes 6 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_6_1_32.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 32 --reuploads 1 --classes 7 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_7_1_32.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 32 --reuploads 1 --classes 8 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_8_1_32.log &
+nohup python mnist4x4_dru.py --qubits 10 --layers 32 --reuploads 1 --classes 9 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_9_1_32.log &
+sleep 1h
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 8 --reuploads 4 --classes 2 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_2_zzry_4_8.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 8 --reuploads 4 --classes 3 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_3_zzry_4_8.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 8 --reuploads 4 --classes 4 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_4_zzry_4_8.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 8 --reuploads 4 --classes 5 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_5_zzry_4_8.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 8 --reuploads 4 --classes 6 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_6_zzry_4_8.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 8 --reuploads 4 --classes 7 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_7_zzry_4_8.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 8 --reuploads 4 --classes 8 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_8_zzry_4_8.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 8 --reuploads 4 --classes 9 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_9_zzry_4_8.log &
+sleep 1h
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 32 --reuploads 1 --classes 2 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_2_zzry_1_32.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 32 --reuploads 1 --classes 3 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_3_zzry_1_32.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 32 --reuploads 1 --classes 4 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_4_zzry_1_32.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 32 --reuploads 1 --classes 5 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_5_zzry_1_32.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 32 --reuploads 1 --classes 6 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_6_zzry_1_32.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 32 --reuploads 1 --classes 7 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_7_zzry_1_32.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 32 --reuploads 1 --classes 8 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_8_zzry_1_32.log &
+nohup python mnist4x4_dru_zzry.py --qubits 10 --layers 32 --reuploads 1 --classes 9 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_9_zzry_1_32.log &
+sleep 1h
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 8 --reuploads 4 --classes 2 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_2_zxz_4_8.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 8 --reuploads 4 --classes 3 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_3_zxz_4_8.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 8 --reuploads 4 --classes 4 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_4_zxz_4_8.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 8 --reuploads 4 --classes 5 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_5_zxz_4_8.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 8 --reuploads 4 --classes 6 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_6_zxz_4_8.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 8 --reuploads 4 --classes 7 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_7_zxz_4_8.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 8 --reuploads 4 --classes 8 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_8_zxz_4_8.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 8 --reuploads 4 --classes 9 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_9_zxz_4_8.log &
+sleep 1h
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 32 --reuploads 1 --classes 2 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_2_zxz_1_32.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 32 --reuploads 1 --classes 3 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_3_zxz_1_32.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 32 --reuploads 1 --classes 4 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_4_zxz_1_32.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 32 --reuploads 1 --classes 5 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_5_zxz_1_32.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 32 --reuploads 1 --classes 6 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_6_zxz_1_32.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 32 --reuploads 1 --classes 7 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_7_zxz_1_32.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 32 --reuploads 1 --classes 8 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_8_zxz_1_32.log &
+nohup python mnist4x4_dru_zxz.py --qubits 10 --layers 32 --reuploads 1 --classes 9 --dataset fashion4x4 > logs_fashion/dru.10.fashion4x4_9_zxz_1_32.log &
